42f0e91931c4ddde070fe1a9b2ebbc397c85815a
Juntakk <nick.habashi@gmail.com>
2024-01-16 16:49:47 -0500

    Animation fix

--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -1,14 +1,16 @@
+import { useEffect } from "react"
 import contacts from "./data"
 import "./contact.css"
 
 const Contact = () => {
+
     return (
         <section id='contact'>
             <h2>Get in touch</h2>
             <p>
                 Shoot me a message via any of the links below
             </p>
-            <div className="container contact__container" data-aos="fade-up">
+            <div className="container contact__container">
                 {
                     contacts.map(contact => <a key={contact.id} href={contact.link} target="_blank" rel="noopener noreferrer">{contact.icon}</a>)
                 }
