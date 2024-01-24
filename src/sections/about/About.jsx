7145d0bf63d8c4830ece47ff42486a9e22e2aefc
Juntakk <nick.habashi@gmail.com>
2024-01-23 21:20:51 -0500

    Favicon

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -1,37 +1,45 @@
-import AboutImage from "../../assets/header.jpg"
-import CV from "../../assets/CV.pdf"
+import AboutImage from "../../assets/header.jpg";
+import CV from "../../assets/CV.pdf";
 import { LiaCloudDownloadAltSolid } from "react-icons/lia";
-import data from "./data"
-import Card from "../../components/Card"
-import "./about.css"
+import data from "./data";
+import Card from "../../components/Card";
+import "./about.css";
 
 const About = () => {
-    return (
-        <section id='about' data-aos="fade-in">
-            <div className="container about__container">
-                <div className="about__left">
-                    <h2>About me</h2>
-                    <div className="about__cards">
-                        {
-                            data.map(item => (
-                                <Card key={item.id} className="about__card">
-                                    <span className="about__card-icon">{item.icon}</span>
-                                    <h5>{item.title}</h5>
-                                    <small>{item.desc}</small>
-                                </Card>
-                            ))
-                        }
-                    </div>
-                    <p>Hey there! I'm a creative soul passionate about bringing ideas to life through coding. I thrive on crafting digital experiences—whether it's websites, apps, or games. I've savored them all, and I'm eager to share the taste. Let's embark on the exciting journey of technology together!</p>                    <a href={CV} download className="btn primary">Download CV<LiaCloudDownloadAltSolid /></a>
-                </div>
-                <div className="about__right">
-                    <div className="about__portrait">
-                        <img src={AboutImage} alt="About image" />
-                    </div>
-                </div>
-            </div>
-        </section>
-    )
-}
+  return (
+    <section id="about" data-aos="fade-in">
+      <div className="container about__container">
+        <div className="about__left">
+          <h2>About me</h2>
+          <div className="about__cards">
+            {data.map((item) => (
+              <Card key={item.id} className="about__card">
+                <span className="about__card-icon">{item.icon}</span>
+                <h5>{item.title}</h5>
+                <small>{item.desc}</small>
+              </Card>
+            ))}
+          </div>
+          <p>
+            Hey there! I'm a creative soul passionate about bringing ideas to
+            life through coding. I thrive on crafting digital
+            experiences—whether it's websites, apps, or games. I've savored them
+            all, and I'm eager to share the taste. Let's embark on the exciting
+            journey of technology together!
+          </p>{" "}
+          <a href={CV} download className="btn primary">
+            Download CV
+            <LiaCloudDownloadAltSolid />
+          </a>
+        </div>
+        <div className="about__right">
+          <div className="about__portrait">
+            <img src={AboutImage} alt="About" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default About
+export default About;
