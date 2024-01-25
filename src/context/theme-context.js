5273fbc0abcf185e2e61dcfe0d3235e28af3f5f7
Juntakk <nick.habashi@gmail.com>
2024-01-25 00:58:56 -0500

    CV change

--- src/context/theme-context.js
+++ src/context/theme-context.js
@@ -1,27 +1,36 @@
-import { createContext, useContext, useReducer, useEffect } from "react"
-import themeReducer from "./themeReducer"
-
+import { createContext, useContext, useReducer, useEffect } from "react";
+import themeReducer from "./themeReducer";
 
 export const ThemeContext = createContext();
 
-const initialThemeState = JSON.parse(localStorage.getItem("themeSettings")) || { primary: "color-1", background: "bg-1" };
+const initialThemeState = JSON.parse(localStorage.getItem("themeSettings")) || {
+  primary: "color-1",
+  background: "bg-2",
+};
 
 export const ThemeProvider = ({ children }) => {
-    const [themeState, dispatchTheme] = useReducer(themeReducer, initialThemeState);
+  const [themeState, dispatchTheme] = useReducer(
+    themeReducer,
+    initialThemeState
+  );
 
-    const themeHandler = (buttonClassName) => {
-        dispatchTheme({ type: buttonClassName })
-    }
+  const themeHandler = (buttonClassName) => {
+    dispatchTheme({ type: buttonClassName });
+  };
 
-    //Save theme settings to local storage
-    useEffect(() => {
-        localStorage.setItem("themeSettings", JSON.stringify(themeState))
-    }, [themeState.primary, themeState.background])
+  //Save theme settings to local storage
+  useEffect(() => {
+    localStorage.setItem("themeSettings", JSON.stringify(themeState));
+  }, [themeState.primary, themeState.background]);
 
-    return <ThemeContext.Provider value={{ themeState, themeHandler }}>{children}</ThemeContext.Provider>
-}
+  return (
+    <ThemeContext.Provider value={{ themeState, themeHandler }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
 
 //Hook to use Theme context wherever we want
 export const useThemeContext = () => {
-    return useContext(ThemeContext);
-}
+  return useContext(ThemeContext);
+};
