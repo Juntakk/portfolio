2b62376a72e0a945b0b63566638b3944b9310621
Juntakk <nick.habashi@gmail.com>
2024-02-01 00:14:40 -0500

    Touch-Ups

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./sections/navbar/Navbar";
 import Header from "./sections/header/Header";
 import About from "./sections/about/About";
+import Certificates from "./sections/certificates/Certificates";
 import Services from "./sections/services/Services";
 import Portfolio from "./sections/portfolio/Portfolio";
 // import Testimonials from './sections/testimonials/Testimonials';
@@ -57,6 +58,7 @@ const App = () => {
       <Navbar />
       <Header />
       <About />
+      {/* <Certificates /> */}
       <Services />
       <Portfolio />
       <Contact />
