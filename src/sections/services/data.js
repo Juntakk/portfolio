a18e402f4923261494a06652d81e4bb6cf252e25
Juntakk <nick.habashi@gmail.com>
2024-02-19 11:32:55 -0500

    Small change

--- src/sections/services/data.js
+++ src/sections/services/data.js
@@ -1,39 +1,61 @@
 import { BsAndroid2 } from "react-icons/bs";
 import { FaCircleNodes } from "react-icons/fa6";
 import { SiCsharp } from "react-icons/si";
-import { FaHtml5 } from "react-icons/fa";
+import { DiRuby } from "react-icons/di";
 import { SiPhp } from "react-icons/si";
 import { FaReact } from "react-icons/fa";
 import { SiCplusplus } from "react-icons/si";
 import { SiPython } from "react-icons/si";
 
-
 const data = [
-    {
-        id: 1, icon: <BsAndroid2 />, title: "Java / Android Studio", desc: "Proficient in Java and Android Studio, I create Android applications with a focus on simplicity, efficiency and user-experience"
-    },
-    {
-        id: 2, icon: <FaCircleNodes />, title: "JavaScript  / Node.js / MongoDB", desc: "Using JavaScript, I built interactive websites and backend API's and with Node.js, I handled server-side tasks, creating efficient and scalable applications."
-    },
-    {
-        id: 3, icon: <SiCsharp />, title: "C# / ASP.net", desc: "I have built websites and applications using C#, ASP.NET, and SQL. With C#, I've added dynamic features and made Visual Studio 2022 my playground"
-    },
-    {
-        id: 4, icon: <FaHtml5 />, title: "HTML / CSS", desc: "Getting better by the day, I shape the foundation and style of web content. With their structure and design finesse, I create appealing and user-friendly websites."
-    },
-    {
-        id: 5, icon: <SiPhp />, title: "PHP", desc: "With PHP, I seamlessly integrate databases for dynamic functionality, enhancing user experience and managing dynamic databases."
-    },
-    {
-        id: 6, icon: <FaReact />, title: "React.js", desc: "New to React.js, I fell in love while crafting my portfolio. Excited about its simplicity, I'm eager to continue exploring this powerful framework."
-    },
-    {
-        id: 7, icon: <SiCplusplus />, title: "C++", desc: "Currently learning C++ during my free time, I'm excited to explore its basics, OOP structure and use it for crafting small video games."
-    },
-    {
-        id: 8, icon: <SiPython />, title: "Python", desc: "Exploring Python is fascinating. Learning in my free time, I've made small apps and can't wait to delve deeper into its possibilities."
-    },
+  {
+    id: 1,
+    icon: <BsAndroid2 />,
+    title: "JAVA",
+    desc: "Proficient in Java and Android Studio, I create Android applications with a focus on simplicity, efficiency and user-experience",
+  },
+  {
+    id: 2,
+    icon: <FaCircleNodes />,
+    title: "JavaScript",
+    desc: "Using JavaScript, I built interactive websites and backend API's and with Node.js, I handled server-side tasks, creating efficient and scalable applications.",
+  },
+  {
+    id: 3,
+    icon: <SiCsharp />,
+    title: "C#",
+    desc: "I have built websites and applications using C#, ASP.NET, and SQL. With C#, I've added dynamic features and made Visual Studio 2022 my playground",
+  },
+  {
+    id: 4,
+    icon: <DiRuby />,
+    title: "Ruby / Ruby on Rails",
+    desc: "Getting better by the day, I write clean and elegant Ruby to build robust and scalable web applications efficiently using object-oriented programming.",
+  },
+  {
+    id: 5,
+    icon: <SiPhp />,
+    title: "PHP",
+    desc: "With PHP, I seamlessly integrate databases for dynamic functionality, enhancing user experience and managing dynamic databases.",
+  },
+  {
+    id: 6,
+    icon: <FaReact />,
+    title: "React.js",
+    desc: "New to React.js, I fell in love while crafting my portfolio. Excited about its simplicity, I'm eager to continue exploring this powerful framework.",
+  },
+  {
+    id: 7,
+    icon: <SiCplusplus />,
+    title: "C++",
+    desc: "Currently learning C++ during my free time, I'm excited to explore its basics, OOP structure and use it for crafting small video games.",
+  },
+  {
+    id: 8,
+    icon: <SiPython />,
+    title: "Python",
+    desc: "Exploring Python is fascinating. Learning in my free time, I've made small apps and can't wait to delve deeper into its possibilities.",
+  },
+];
 
-]
-
-export default data
+export default data;
