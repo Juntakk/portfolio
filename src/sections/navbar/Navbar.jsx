269cfe0b265dafd7f1ae075227131fce0c60e9f8
Juntakk <nick.habashi@gmail.com>
2024-05-06 10:35:46 -0400

    CV chanche

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -5,8 +5,8 @@ import "./navbar.css";
 import { useModalContext } from "../../context/modal-context";
 import { useLanguage } from "../../theme/LanguageContext";
 import LanguageToggle from "../../theme/LanguageToggle";
-import CV from "../../assets/CV_V2_R.pdf";
-import CV_FR from "../../assets/CV_V2_FR_R.pdf";
+import CV from "../../assets/CV_EN_R.pdf";
+import CV_FR from "../../assets/CV_FR_R.pdf";
 import { LiaCloudDownloadAltSolid } from "react-icons/lia";
 
 const Navbar = () => {
