04d2d1f7a798cd157c8727377bada1f67d3f4303
Juntakk <nick.habashi@gmail.com>
2024-09-08 17:27:12 -0400

    d

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
 import "./card.css";
 
 const Card = ({ children, className, onClick }) => {
+
   return (
     <article className={`card ${className}`} onClick={onClick}>
       {children}
--- src/sections/portfolio/Project.jsx
+++ src/sections/portfolio/Project.jsx
@@ -1,55 +1,73 @@
 import Card from "../../components/Card";
 
 const Project = ({ project, data }) => {
+
+  let url = data[project.id - 1].demo.startsWith("http")
+    ? data[project.id - 1].demo
+    : data[project.id - 1].github;
+
+  let handleDivClick = () => {
+    window.open(url, "_blank");
+  };
+
   return (
-    <Card className="portfolio__project">
-      <div className="portfolio__project-image">
-        {data[project.id - 1].demo.startsWith("http") ? (
+    <>
+      <div>
+        <Card className="portfolio__project" onClick={handleDivClick}>
+          <div className="portfolio__project-image">
+            {data[project.id - 1].demo.startsWith("http") ? (
+              <a
+                href={data[project.id - 1].demo}
+                target="_blank"
+                rel="noopner noreferrer"
+              >
+                {" "}
+                <img src={data[project.id - 1].image} alt="Portfolio Project" />
+              </a>
+            ) : (
+              <>
+                <a
+                  href={data[project.id - 1].github}
+                  target="_blank"
+                  rel="noopner noreferrer"
+                >
+                  <img
+                    src={data[project.id - 1].image}
+                    alt="Portfolio Project"
+                  />
+                </a>
+              </>
+            )}
+          </div>
+          <div className="titleAndDesc">
+            <h4 className="project-title">{data[project.id - 1].title}</h4>
+            <span className="project-desc">{data[project.id - 1].desc}</span>
+          </div>
+        </Card>
+        <div className="portfolio__project-cta">
           <a
-            href={data[project.id - 1].demo}
-            target="_blank"
-            rel="noopner noreferrer"
-          >
-            {" "}
-            <img src={data[project.id - 1].image} alt="Portfolio Project" />
-          </a>
-        ) : (
-          <>
-            <a
-              href={data[project.id - 1].github}
-              target="_blank"
-              rel="noopner noreferrer"
-            >
-              <img src={data[project.id - 1].image} alt="Portfolio Project" />
-            </a>
-          </>
-        )}
-      </div>
-      <h4 className="project-title">{data[project.id - 1].title}</h4>
-      <span className="project-desc">{data[project.id - 1].desc}</span>
-      <div className="portfolio__project-cta">
-        <a
-          href={data[project.id - 1].github}
-          className="btn sm primary"
-          target="_blank"
-          rel="noopner noreferrer"
-        >
-          Github
-        </a>
-        {data[project.id - 1].demo.startsWith("http") ? (
-          <a
-            href={data[project.id - 1].demo}
+            href={data[project.id - 1].github}
             className="btn sm primary"
             target="_blank"
             rel="noopner noreferrer"
           >
-            Demo
+            Github
           </a>
-        ) : (
-          ""
-        )}
+          {data[project.id - 1].demo.startsWith("http") ? (
+            <a
+              href={data[project.id - 1].demo}
+              className="btn sm primary"
+              target="_blank"
+              rel="noopner noreferrer"
+            >
+              Demo
+            </a>
+          ) : (
+            ""
+          )}
+        </div>
       </div>
-    </Card>
+    </>
   );
 };
 
