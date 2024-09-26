3b1109f97e91c8086b637e4f38de46b15cf0f8da
Juntakk <nick.habashi@gmail.com>
2024-09-26 19:33:14 -0400

    Update data.js

--- src/sections/contact/data.js
+++ src/sections/contact/data.js
@@ -3,10 +3,13 @@ import { RiMessengerLine } from "react-icons/ri"
 import { FaWhatsapp } from "react-icons/fa"
 
 const data = [
-    { id: 1, icon: <HiOutlineMail />, link: "mailto:nick.habashi@gmail.com" },
-    { id: 2, icon: <RiMessengerLine />, link: "https://www.messenger.com/t/100009499215020" },
-    { id: 3, icon: <FaWhatsapp />, link: "https://wa.me/+15142925985" }
-]
+  { id: 1, icon: <HiOutlineMail />, link: "mailto:nick.habashi@gmail.com" },
+  {
+    id: 2,
+    icon: <RiMessengerLine />,
+    link: "https://www.messenger.com/t/100009499215020",
+  },
+];
 
 export default data
 
