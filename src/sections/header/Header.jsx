8214974551a35aaa90c2e89df4b8c67fa0582f93
Nicolas Gauthier <nick.habashi@gmail.com>
2024-12-19 11:05:36 -0500

    particles

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
 import data from "./data";
 import AOS from "aos";
@@ -5,17 +6,42 @@ import "aos/dist/aos.css";
 import "particles.js";
 import "./header.css";
 import { useLanguage } from "../../theme/LanguageContext";
+import { useThemeContext } from "../../context/theme-context";
 
 const Header = () => {
   const { language } = useLanguage();
+  const themeContext = useThemeContext();
+  const [particleColor, setParticleColor] = useState(null); // Initialize as null
 
+  const colorMap = {
+    "color-1": "#ffab91", // Soft coral to contrast the grayish tones
+    "color-2": "#d4e157", // Bright lime to pop against the greens
+    "color-3": "#f48fb1", // Light pink to complement the warm pinkish tones
+    "color-4": "#ff8a65", // Warm orange for a vibrant contrast to the reds
+    "color-5": "#81d4fa", // Light cyan to contrast the blue-gray tones
+    "color-6": "#aed581", // Fresh green to offset the teal-green tones
+  };
+
+  const handleParticleColor = (theme) => colorMap[theme] || "#CCCCCC"; // Default color
+
+  // Update particle color when the theme changes
+  useEffect(() => {
+    if (themeContext?.themeState?.primary) {
+      const newColor = handleParticleColor(themeContext.themeState.primary);
+      setParticleColor(newColor);
+    }
+  }, [themeContext.themeState.primary]);
+
+  // Initialize particlesJS when the particleColor is set
   useEffect(() => {
     AOS.init({ duration: 2000 });
 
+    if (!particleColor) return; // Ensure particleColor is valid
+
     window.particlesJS("particles-js", {
       particles: {
-        number: { value: 90, density: { enable: true, value_area: 800 } },
-        color: { value: "#88988E" },
+        number: { value: 10, density: { enable: true, value_area: 700 } },
+        color: { value: particleColor },
         shape: {
           type: "circle",
           stroke: { width: 0, color: "#000000" },
@@ -23,24 +49,24 @@ const Header = () => {
         },
         opacity: {
           value: 0.8,
-          random: false,
-          anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
+          random: true,
+          anim: { enable: true, speed: 0.1, opacity_min: 0.1, sync: false },
         },
         size: {
-          value: 3,
+          value: 105,
           random: false,
-          anim: { enable: false, speed: 40, size_min: 20, sync: false },
+          anim: { enable: false, speed: 40, size_min: 10, sync: false },
         },
         line_linked: {
-          enable: true,
-          distance: 150,
-          color: "#87837c",
+          enable: false,
+          distance: 15,
+          color: particleColor,
           opacity: 0.4,
           width: 1,
         },
         move: {
           enable: true,
-          speed: 4,
+          speed: 1,
           direction: "none",
           random: true,
           straight: false,
@@ -52,14 +78,14 @@ const Header = () => {
       interactivity: {
         detect_on: "canvas",
         events: {
-          onhover: { enable: false, mode: "repulse" },
+          onhover: { enable: false, mode: "bubble" },
           onclick: { enable: false, mode: "push" },
-          resize: false,
+          resize: true,
         },
         modes: {
           grab: { distance: 400, line_linked: { opacity: 1 } },
           bubble: {
-            distance: 400,
+            distance: 200,
             size: 40,
             duration: 2,
             opacity: 8,
@@ -72,7 +98,7 @@ const Header = () => {
       },
       retina_detect: false,
     });
-  }, []);
+  }, [particleColor]);
 
   const [isHovered, setIsHovered] = useState(false);
 
@@ -131,12 +157,12 @@ const Header = () => {
         <h1 className="myName">
           <span className="typing">Nicolas Gauthier</span>
         </h1>
-        <p data-aos="fade-up">
+        {/* <p data-aos="fade-up" className="desc_p">
           {language === "en"
             ? "Proficient in Web and Mobile Development, Ready to Contribute to Innovative Teams and Challenging Projects."
             : "Compétent en développement web et mobile, prêt à contribuer à des équipes innovantes et à des projets stimulants."}
           <br />
-        </p>
+        </p> */}
         <div className="header__cta" data-aos="fade-up">
           <a href="#contact" className="btn primary">
             {language === "en" ? "Let's Talk" : "Discutons"}
