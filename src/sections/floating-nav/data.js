94b2f994591bdc4487834b65e6aea3c1d77f0a79
Nicolas Gauthier <nick.habashi@gmail.com>
2024-12-19 12:38:31 -0500

    Update data.js

--- src/sections/floating-nav/data.js
+++ src/sections/floating-nav/data.js
@@ -5,8 +5,8 @@ import { TbMessageDots } from "react-icons/tb";
 
 const data = [
   { id: 1, link: "#", icon: <HiHome /> },
-  { id: 2, link: "#services", icon: <FaGlobe /> },
-  { id: 3, link: "#portfolio", icon: <PiShareNetworkBold /> },
+  { id: 2, link: "#portfolio", icon: <PiShareNetworkBold /> },
+  { id: 3, link: "#services", icon: <FaGlobe /> },
   { id: 4, link: "#contact", icon: <TbMessageDots /> },
 ];
 
